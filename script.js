b0b29c03bfd4baae899c2f277f4d8bc3e2393be1
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-11-05 20:34:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let unsavedChanges = false;
+
+// Add a listener for changes in the workspace that indicate unsaved progress
+document.querySelector('.workspace').addEventListener('change', () => {
+    unsavedChanges = true;
+});
+
+// Warn the user about unsaved changes when they attempt to close the tab
+window.addEventListener('beforeunload', (e) => {
+    if (unsavedChanges) {
+        e.preventDefault();
+        e.returnValue = '';
+    }
+});
+
 document.getElementById('import-btn').addEventListener('click', () => {
     // Implement import functionality
     alert('Import functionality not implemented.');
