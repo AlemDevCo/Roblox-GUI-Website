90b2a675b872fc311f175ef91d2825d23155ea7e
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-11-05 20:32:30 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('import-btn').addEventListener('click', () => {
+    // Implement import functionality
+    alert('Import functionality not implemented.');
+});
+
+document.getElementById('export-btn').addEventListener('click', () => {
+    // Implement export functionality
+    alert('Export functionality not implemented.');
+});
